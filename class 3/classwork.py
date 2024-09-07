6661825f6bf9f328b6e177f17d0d7e293dffc625
dikshyant77 <rayamajhidikshyant@gmail.com>
2024-09-07 12:03:55 +0545

    Add files via upload

---
+++ class 3/classwork.py
@@ -0,0 +1,32 @@
+name="Dikshyant"
+print("data type of name is ", type(name))
+age=13
+print("data type of age is ",type(age))
+num=12.3
+print("data type of num is ",type(num))
+a=True
+print("data type of a is ",type(a))
+print()
+#user input
+# name=input("enter your name ")
+# print("wow nice name!",name)
+# print(type(name))
+
+# #ask the user to enter age
+# age=int(input("enter your age "))
+# print(age)
+# print(type(age))
+
+fruit="Apple"
+#indexing
+print(fruit[0])
+print(fruit[1])
+print(fruit[4])
+print(fruit[-1])
+
+#sclicing
+print(fruit[0:3])
+#reverse
+print(fruit[:3])
+print(fruit[:])
+print(fruit[::-1])
---
+++ class 3/main.py
@@ -0,0 +1,3 @@
+word = "congratulations to my friend for winning the gold medal in state game"
+print(word.upper())
+print(type(word))
